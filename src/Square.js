792620676c0d7e756de9a55346b86c5c179e0c7e
Paul Felton <paul.r.felton@gmail.com>
2020-04-15 10:18:47 -0500

    Starting to print out the 9x9 board

---
+++ src/Square.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+//Using console.log and F12 way nicer than alert for msgs and vals
+
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        X
+      </button>
+    );
+  }
+}
+export default Square;
